5446f50a5ca38d6d8934fd0bbff04516d739c816
Shubhra Sarker <ssuvro22@gmail.com>
2021-09-20 18:43:11 -0700

    Init

---
+++ Assets/PlayerAim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+    {
+
+        [SerializeField] private float turnSpeed = 10;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+    
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float camRot = Camera.main.transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(0,camRot,0), turnSpeed*Time.deltaTime);
+        }
+        
+    }
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 0.5f;
+    public float speed = 1;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        float jumpInp = Input.GetAxis("Jump");
+        float crouch = Input.GetAxis("Crouch");
+
+        int sprint = Input.GetAxis("Sprint") > 0.5f ? 1 : 0;
+        
+        // from zero to 2;
+        float finalSpeedX = inputX * walkSpeed + inputX * speed * sprint;
+        float finalSpeedY = inputY * walkSpeed + inputY * speed * sprint;
+        
+        /*anim.SetFloat("SpeedX",finalSpeedX);
+        anim.SetFloat("SpeedY",finalSpeedY);
+        anim.SetFloat("JumpSpeed",jumpInp);
+        anim.SetFloat("Crouch",crouch);
+        */
+    }
+}
